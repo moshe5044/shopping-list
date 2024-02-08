8154368a61cbda829458ee54863874b9dec83f1b
moshe <moshehadar001@gmail.com>
2024-02-08 18:10:06 +0200

    set the popup component(Modal)

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -3,36 +3,36 @@ import style from "./style.module.css";
 import Modal from "../Modal";
 
 export default function Category({ category }) {
-    const [openModal, setOpenModal] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-    const handleItemClick = (item) => {
-        setSelectedItem(item);
-        setOpenModal(true);
-    };
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setOpenModal(true);
+  };
 
-    const closeModal = () => {
-        setSelectedItem(null);
-        setOpenModal(false);
-    };
+  const closeModal = () => {
+    setSelectedItem(null);
+    setOpenModal(false);
+  };
 
-    return (
-        <div className={style["category"]}>
-            <button className={style["plus"]}>+</button>
-            {category.map((item, index) => (
-                <div className={style["category-item"]} key={index}>
-                    <img
-                        className={style["img-item"]}
-                        src={item.image}
-                        alt={`Image ${index}`}
-                        onClick={() => handleItemClick(item)}
-                    />
-                </div>
-            ))}
-
-            {openModal && (
-                <Modal selectedItem={selectedItem} closeModal={closeModal} />
-            )}
+  return (
+    <div className={style["category"]} id="category">
+      <button className={style["plus"]}>+</button>
+      {category.map((item, index) => (
+        <div className={style["category-item"]} key={index}>
+          <img
+            className={style["img-item"]}
+            src={item.image}
+            alt={`Image ${index}`}
+            onClick={() => handleItemClick(item)}
+          />
         </div>
-    );
+      ))}
+
+      {openModal && (
+        <Modal selectedItem={selectedItem} closeModal={closeModal} />
+      )}
+    </div>
+  );
 }
--- src/components/ItemInSearch/index.jsx
+++ src/components/ItemInSearch/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import style from "./style.module.css";
 
-
 export default function ItemInSearch({ item }) {
-
   return (
     <div className={style["item"]}>
-      <div className={style["emoji"]}>{item.emoji}</div>
+      <div className={style["photo"]}>
+        <img className={style["img-item"]} src={item.image} />
+      </div>
       <div className={style["name"]}>{item.name}</div>
-      <div className={style["category"]}>{item.category}</div>
     </div>
   );
 }
